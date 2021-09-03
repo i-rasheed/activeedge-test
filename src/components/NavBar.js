89a061375cf7e7a2dc1f911a3f664557437ade7d
Rasheed <abdulrasheedozovehe@gmail.com>
2021-09-03 09:06:37 +0100

    styled navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,14 +5,14 @@ export default function NavBar() {
     return (
         <div>
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <Link to='/'>HOME</Link>
+                <Link className="nav-link" to='/'>HOME</Link>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
                     <li class="nav-item active">
-                        <Link to='/album'>Albums<span class="sr-only">(current)</span></Link>
+                        <Link className="nav-link" to='/album'>Albums<span class="sr-only">(current)</span></Link>
                     </li>
                     </ul>
                 </div>
